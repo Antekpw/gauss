dc8a3ff88b2985b387f5ebe63b91812fa205e1c5
Antoni Pietras <antosiek007@gmail.com>
2023-12-20 14:16:50 +0100

    Eliminacja gaussa rozszerzona o wybór elementu głównego

--- src/gauss.c
+++ src/gauss.c
@@ -8,16 +8,27 @@ int eliminate(Matrix *mat, Matrix *b){
     /**
   	 * Tutaj należy umieścić właściwą implemntację.
 		 */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
 	double wsp;
 	for(int i=0; i<mat->r-1; i++)
-	{
+	{ 
+	
 		for(int j=i+1; j<mat->r; j++)
 		{
-		if(mat->data[i][i]==0) return 1;       
+		if(mat->data[i][i]==0) {
+			return 1;
+		}
+		int max = i;
+		if(mat->data[j][i] > mat->data[max][i]){
+			max = j;
+		}
+		if(max!=i){
+			double *temp = mat->data[i];
+			mat->data[i] = mat->data[max];
+			mat->data[max] = temp;
+			double b2 = b->data[max][0];
+			b->data[max][0] = b->data[i][0];
+			b->data[i][0] = b2;
+		}	
 			wsp = mat->data[j][i] / mat->data[i][i];
 		for(int k=i; k<mat->c; k++)
 		mat->data[j][k] -=wsp * mat->data[i][k]; 
