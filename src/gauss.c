f2b60596dcce269c4fd2134b014dc169b6634f1e
Antoni Pietras <antosiek007@gmail.com>
2023-12-20 13:33:48 +0100

    Student B implementuje metode eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -8,7 +8,24 @@ int eliminate(Matrix *mat, Matrix *b){
     /**
   	 * Tutaj należy umieścić właściwą implemntację.
 		 */
-/*uff*/
+int eliminate(Matrix *mat, Matrix *b){
+    /**
+  	 * Tutaj należy umieścić właściwą implemntację.
+		 */
+	double wsp;
+	for(int i=0; i<mat->r-1; i++)
+	{
+		for(int j=i+1; j<mat->r; j++)
+		{
+		if(mat->data[i][i]==0) return 1;       
+			wsp = mat->data[j][i] / mat->data[i][i];
+		for(int k=i; k<mat->c; k++)
+		mat->data[j][k] -=wsp * mat->data[i][k]; 
+		b->data[j][0] -= wsp * b->data[i][0]; 
+		}
+	
+		}
+
 		return 0;
 }
 
